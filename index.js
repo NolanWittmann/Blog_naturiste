8e6df4a2e90bd5a5e7c217dba1cb04a3d608bbe1
Nolan Wittmann <wittmann.nolan@gmail.com>
2022-01-04 19:27:55 +0100

    Ajout de fonction JS et modification de la page cliché

--- index.js
+++ index.js
@@ -6,7 +6,25 @@ menuButton.addEventListener('click',function(){
   menu.classList.toggle('show-menu');
   menuButton.classList.toggle('close');
 });
+// show or hide
+
+//page partir
+const bouton = document.querySelector(".btnJs")
+const pop = document.querySelector(".agdeP")
+let visible = false
+
+bouton.addEventListener("click", ()=> {
+    visible = !visible
+    visible ? pop.classList.add("visibles") : pop.classList.remove("visibles")
+})
+
+let visibles
+const bouton1 = document.querySelector(".btnJs1")
+const pop1 = document.querySelector(".chmP")
 
 
-
-
+bouton1.addEventListener("click", ()=> {
+  visibles = !visibles
+  visibles ? pop1.classList.add("visible1") : pop1.classList.remove("visible1")
+})
+//page partir
